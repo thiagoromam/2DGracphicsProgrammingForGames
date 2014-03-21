2e9ca5998b9983babace4173e887781a840bbc3e
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2014-03-20 23:37:04 -0300

    blur test

--- GeneralTest/TestComponent.cs
+++ GeneralTest/TestComponent.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Resources;
 using Color = Microsoft.Xna.Framework.Color;
 
 // ReSharper disable once CheckNamespace
@@ -11,12 +12,13 @@ namespace Core
     public class TestComponent
     {
         private readonly MainGame _game;
-        private Texture2D _texture;
-        private Texture2D _snowman;
-        private Texture2D _snowmanInverted;
-
-        private Vector2 _position;
-        private Vector2 _velocity;
+        private int _screenWidth;
+        private int _screenHeight;
+        private Texture2D _snowAssets;
+        private Rectangle _snowmanSource;
+        private Vector2 _snowmanOrigin;
+        private Vector2[] _positions;
+        private Vector2 _centerOfScreen;
 
         public TestComponent(MainGame game)
         {
@@ -26,78 +28,85 @@ namespace Core
 
         public void Initialize()
         {
-            _position = Vector2.Zero;
-            _velocity = new Vector2(1, 1.5f);
+            var viewport = _game.GraphicsDevice.Viewport;
+            _screenWidth = viewport.Width;
+            _screenHeight = viewport.Height;
+            _centerOfScreen = viewport.CalculateCenterOfScreen();
         }
 
         public void LoadContent(ContentManager content)
         {
-            _texture = content.Load<Texture2D>("snow_assets");
+            _snowAssets = content.Load<Texture2D>("snow_assets");
+            _snowmanSource = new Rectangle(0, 128, 256, 256);
+            _snowmanOrigin = new Vector2(_snowmanSource.Width / 2f, _snowmanSource.Height / 2f);
 
-            var source = new Rectangle(0, 128, 256, 256);
-            var colors = new Color[source.Width * source.Height];
-            _texture.GetData(0, source, colors, 0, (source.Width * source.Height));
+            _positions = new Vector2[9];
+            var offset = new Vector2(_centerOfScreen.X - 500, _centerOfScreen.Y - 250);
+            for (var i = 0; i < 3; i++)
+            {
+                var x = offset.X + i * 500;
+                for (var j = 0; j < 3; j++)
+                {
+                    var y = offset.Y + j * 250;
 
-            _snowman = new Texture2D(_game.GraphicsDevice, source.Width, source.Height);
-            _snowmanInverted = new Texture2D(_game.GraphicsDevice, source.Width, source.Height);
-
-            _snowman.SetData(colors);
-            _snowmanInverted.SetData(colors);
+                    _positions[i + (3 * j)] = new Vector2(x, y);
+                }
+            }
         }
 
         public void Update(GameTime gameTime)
         {
-            UpdatePosition();
-            UpdatePixel();
-        }
 
-        private void UpdatePosition()
-        {
-            _position += _velocity;
-
-            if (_position.X < 0 || _position.X > 255)
-                _velocity.X *= -1;
-            if (_position.Y < 0 || _position.Y > 255)
-                _velocity.Y *= -1;
-
-            _position.X = MathHelper.Clamp(_position.X, 0, 255);
-            _position.Y = MathHelper.Clamp(_position.Y, 0, 255);
-        }
-
-        private void UpdatePixel()
-        {
-            const int width = 256;
-            const int height = 256;
-
-            var colors = new Color[width * height];
-            _snowman.GetData(colors);
-
-            for (var i = 0; i < width; ++i)
-            {
-                for (var j = 0; j < height; ++j)
-                {
-                    var index = j + (width * i);
-                    var distance = Math.Sqrt(Math.Pow(i - _position.X, 2) + Math.Pow(j - _position.Y, 2));
-                    const double radius = 50;
-
-                    if (distance < radius)
-                    {
-                        colors[index].R = (byte)(255 -colors[index].R);
-                        colors[index].G = (byte)(255 -colors[index].G);
-                        colors[index].B = (byte)(255 -colors[index].B);
-                        colors[index].A = 255;
-                    }
-                }
-            }
-
-            _snowmanInverted.SetData(colors);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
+            var tempBinding = _game.GraphicsDevice.GetRenderTargets();
+            var tempRenderTarget = new RenderTarget2D(_game.GraphicsDevice, _screenWidth, _screenHeight);
+
+            _game.GraphicsDevice.SetRenderTarget(tempRenderTarget);
+
+            _game.GraphicsDevice.Clear(Color.CornflowerBlue);
             spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.NonPremultiplied);
-            spriteBatch.Draw(_snowman, Vector2.Zero, Color.White);
-            spriteBatch.Draw(_snowmanInverted, new Vector2(256, 0), Color.White);
+            for (var i = 0; i < _positions.Length; ++i)
+            {
+                spriteBatch.Draw(_snowAssets, _positions[i], _snowmanSource, Color.White, 0, _snowmanOrigin, 1, SpriteEffects.None, 1);
+            }
+            spriteBatch.End();
+
+            _game.GraphicsDevice.SetRenderTargets(tempBinding);
+
+            var colors = new Color[_screenWidth * _screenHeight];
+            var maxColorsIndex = colors.Length - 1;
+            tempRenderTarget.GetData(colors);
+
+            for (var i = 0; i < _screenWidth; i++)
+            {
+                for (var j = 0; j < _screenHeight; j++)
+                {
+                    const int blurAmount = 5;
+
+                    var index = i + (_screenWidth * j);
+                    var previousIndex = index - blurAmount;
+                    var nextIndex = index + blurAmount;
+
+                    if (previousIndex < 0 || nextIndex > maxColorsIndex) continue;
+
+                    var currentElement = colors[index];
+                    var previousElement = colors[previousIndex];
+                    var nextElement = colors[nextIndex];
+
+                    colors[index].R = (byte)((previousElement.R + currentElement.R + nextElement.R) / 3.0f);
+                    colors[index].G = (byte)((previousElement.G + currentElement.G + nextElement.G) / 3.0f);
+                    colors[index].B = (byte)((previousElement.B + currentElement.B + nextElement.B) / 3.0f);
+                }
+            }
+
+            var texture = new Texture2D(_game.GraphicsDevice, _screenWidth, _screenHeight);
+            texture.SetData(colors);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(texture, Vector2.Zero, Color.White);
             spriteBatch.End();
         }
     }
